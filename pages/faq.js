fa6d8f7afdc6a47a8726c461c073d4ba76f86890
aubreyarcangel <aarcangel@gmail.com>
2020-10-28 16:45:03 -0400

    added faq content

--- pages/faq.js
+++ pages/faq.js
@@ -27,15 +27,13 @@ export default function FAQ() {
       title: 'What is Platform 327, and how does it help design teams?',
       content: (
         <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
-          accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
-          sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
-          elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita 
-          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor 
-          sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore 
-          et dolore magna
+          Platform 327 is a digital design studio that combines all the resources and tools you need to 
+          design, develop and deliver digital product solutions. 
+        <br/>
+        <br/>
+          Our UX Skills Assessment as well as training 
+          events and courses help design teams evaluate skill sets, build on those skill sets, identify 
+          challenges and solve problems.
         </p>
       ),
       expanded: false,
@@ -46,15 +44,11 @@ export default function FAQ() {
       content: (
         <>
         <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
-          accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
-          sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
-          elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita 
-          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor 
-          sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore 
-          et dolore magna
+          Platform 327 offers a UX Skills Assessment to identify strengths, priorities and interests 
+          and determine areas where you can grow to make the most impact. Our Introduction to UX training 
+          prepares designers to create effective and efficient products that meet the user’s needs. 
+          Our UX Library includes a category specifically dedicated to designers, as well as a bank of 
+          additional learning resources.
         </p>
         </>
       ),
@@ -65,15 +59,8 @@ export default function FAQ() {
       title: 'How can you sign up for sessions?',
       content: (
         <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
-          accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
-          sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
-          elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita 
-          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor 
-          sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore 
-          et dolore magna
+          Visit the Platform 327 page. This will tell you where to start. Select the course you’re 
+          interested in and fill out the form.
         </p>
       ),
       expanded: false,
@@ -83,15 +70,14 @@ export default function FAQ() {
       title: 'What is UX Skills Assessment, and why should I consider taking one?',
       content: (
         <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
-          accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
-          sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
-          elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita 
-          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor 
-          sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore 
-          et dolore magna
+          A UX Skills Assessment will help identify strengths, priorities and interests. 
+          It also identifies areas for growth so that you and your team can make the most 
+          impact. 
+          <br />
+          <br />
+          Human-centered product design can require a broad range of skills, including
+          usability testing, information architecture and visual design. Our skills assessment 
+          will help ensure your team has all the necessary skills to succeed.
         </p>
       ),
       expanded: false,
@@ -101,15 +87,11 @@ export default function FAQ() {
       title: 'What resources are available for UX designers, engineers and project teams?',
       content: (
         <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
-          accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
-          sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
-          elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita 
-          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor 
-          sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore 
-          et dolore magna
+          Check out our BESPIN Apps page and see recent apps developed by designers within BESPIN, 
+          and look at the BESPIN Ecosystem and Case Studies for best UX practices from those projects. 
+          You can also go to our contact page and submit a request for a UX designer or UX design team 
+          for your project.The Platform 327 studio team will get back to you soon for next steps and 
+          recommendations.
         </p>
       ),
       expanded: false,
@@ -119,15 +101,11 @@ export default function FAQ() {
       title: 'How are Platform 327 and Air Force BESPIN related?',
       content: (
         <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
-          accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
-          sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
-          elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita 
-          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor 
-          sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore 
-          et dolore magna
+          Platform 327 is part of the Air Force’s Business Enterprise Systems Program Innovation 
+          (BESPIN) at Maxwell Air Force Base in Montgomery, Alabama. It uses agile development to 
+          quickly implement ideas aimed at improving mission support capabilities. Platform 327 is 
+          a virtual schoolhouse for designers, developers and cross-functional teams learning to 
+          create the digital solutions that will ultimately improve the lives of airmen and women.
         </p>
       ),
       expanded: false,
@@ -137,15 +115,14 @@ export default function FAQ() {
       title: 'I am from an AF Base and/or from another DoD unit interested in finding a team with design and UX capabilities; how can I find a project team?',
       content: (
         <p>
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor 
-          invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et 
-          accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata 
-          sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
-          elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita 
-          kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor 
-          sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore 
-          et dolore magna
+          For UX designers, check out our UX Library page, which houses design and UX resources, 
+          the BESPIN Design System, a link to core Digital UX courses and tracks, and UX toolkits.
+        <br />
+        <br />
+
+          For engineers and project teams, we’ve put together toolkits and guides on how to implement 
+          basic UX practices and testing for design projects. We also provide ongoing workshops and 
+          events on Platform 327.
         </p>
       ),
       expanded: false,
